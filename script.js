06580cbc5472b82e25edfe557946ee6864c7a1ce
Mastur <bikanerwalamastur13@gmail.com>
2025-01-23 11:24:39 +0530

    new add

---
+++ script.js
@@ -0,0 +1,4 @@
+//get element by class
+document.getElementsByClassName('dom-class')[0].innerHTML="<i> All of ypu looking good </i>";
+//get element by id
+document.getElementById('dom-class').innerHTML="<i> All of you looking good </i>"
