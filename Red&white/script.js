c0823c5a6ad28a39b60c82a58735ab9fe7fd1595
Mastur <bikanerwalamastur13@gmail.com>
2025-01-27 11:47:04 +0530

    nothing

--- Red&white/script.js
+++
@@ -1,5 +0,0 @@
-//get element by class
-document.getElementsByClassName('dom-class')[0].innerHTML="<i> All of ypu looking good </i>";
-//get element by id
-document.getElementById('dom-id').innerHTML=" All of you looking good "
-
