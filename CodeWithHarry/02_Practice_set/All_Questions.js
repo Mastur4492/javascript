552316339c14866f46b025900148e0acedf7d72a
Mastur <bikanerwalamastur13@gmail.com>
2025-02-06 13:55:46 +0530

    New Practice set add

---
+++ CodeWithHarry/02_Practice_set/All_Questions.js
@@ -0,0 +1,59 @@
+// 01_Question //
+// Use logical operators to find whether the age of a person lies between 10 and 20 ? //
+
+// let age = parseInt(prompt("What is your age ?"))
+
+// if (age > 10 && age < 20) {
+//     console.log("Your age lies between 10 and 20!");
+// }
+// else {
+//     console.log("Your age doesn't lies between 10 and 20!");
+// }
+
+// 02_Question //
+// Demonstrate the use of switch case statements in javascript //
+
+// let age = prompt("Give me your age to check you are able to drive or not!")
+
+// switch (age) {
+//     case '18' :
+//         console.log("Yes,You are able to drive!");
+//         break;
+//     case '10':
+//         console.log("You are not able to drive!");
+//         break;
+//     default:
+//         console.log("please give me real age"); 
+//         break;
+// }
+
+// 03_Question //
+// Write a javascript program to find whether a number is divisible by 2 and 3. //
+
+// let num = parseInt(prompt("Give me your number!"))
+
+// if (num % 2 == 0 && num % 3 == 0) {
+//     console.log("your number are divisible");
+// }
+// else {
+//     console.log("your number is not divisible");
+// }
+
+// 04_Question //
+// Write a javascript program to find whether a number is divisible by either 2 or 3. //
+
+// let num = parseInt(prompt("Give me your number!"))
+
+// if (num % 2 == 0 || num % 3 == 0) {
+//     console.log("your number are divisible");
+// }
+// else {
+//     console.log("your number is not divisible");
+// }
+
+// 05_Question //
+// Print you can drive or not using ternary operator based on age 18. //
+
+// let age = parseInt(prompt("Enter your age"));
+// let a = age>18?"you can drive":"you can not drive";
+// console.log(a);
