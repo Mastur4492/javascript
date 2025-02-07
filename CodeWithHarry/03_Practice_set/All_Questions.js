042bcbe6934b7bf734ab200ae98ece513599a283
Mastur <bikanerwalamastur13@gmail.com>
2025-02-07 19:43:45 +0530

    new mini project are update but javascript is pending and thired practice set added

---
+++ CodeWithHarry/03_Practice_set/All_Questions.js
@@ -0,0 +1,43 @@
+// 01_Question //
+// Write a program to print the marks of a student in an object using for loop.//
+// obj = {Mastur:98 ,Utkarsh:70 , ajaz:7 } //
+
+// const marks = {
+//     Mastur: 98,
+//     Utkarsh: 70,
+//     ajaz: 70                                
+// }
+// for (let i = 0; i < Object.keys(marks).length; i++) {
+//     console.log("The marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]);
+// }
+
+// 02_Question //
+// Write the program in Q1 using "for-in" loop //
+
+// const marks = {
+//     Mastur: 98,
+//     Utkarsh: 70,
+//     ajaz: 70                                
+// }
+// for (let key in marks) {
+//     console.log("The marks of " + key + " are " + marks[key]);
+// }
+
+// 03_Question //
+// Write a program to print "try again" Until the user enter the correct number. //
+
+// let cn = 50
+// let un
+// while (cn != un) {
+//     console.log("Try again");
+//     un = prompt("enter correct number")
+// }
+// console.log("You have enter correct number");
+
+// 04_question //
+// Write a function to find mean of 5 numbers. //
+
+// const mean = (a,b,c,d)=>{
+//     return ( a + b + c + d)/4
+// }
+// console.log(mean(4,5,6,7));
