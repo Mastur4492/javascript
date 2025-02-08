a828121028e13eb1c8b948beaff7f0922ee6529b
Mastur <bikanerwalamastur13@gmail.com>
2025-02-08 13:45:59 +0530

    04_Practice set added

---
+++ CodeWithHarry/04_Practice_set/All_Questions.js
@@ -0,0 +1,32 @@
+// 01_Question //
+// What will the following print in javascript?//
+// console.log("har\"".length) //
+
+// let str = "mas\""
+
+// console.log(str.length);
+
+// 02_Question //
+// Explore the includes,starts with & ends with functions of a string //
+
+// 03_Question //
+// write a program to convert a given string to lowercase //
+
+// let str = "MASTUR"
+
+// console.log(str.toLowerCase());
+
+// 04_Question //
+// Extract the amount out of this string //
+// "Please give Rs 1000" //
+
+// let str = "please give Rs 1000"
+// let amount = str.slice("plaese give Rs".length)
+// console.log(amount);
+
+// 05_Question // 
+// Try  to change  4 th character of a given string were you able to do it ? //
+
+// let str =  "Mastur"
+// let change = str.replace("t","r")
+// console.log(change);
