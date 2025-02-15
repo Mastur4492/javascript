c38a85dcb043168792568cbc97fcbf37a377c984
Mastur <bikanerwalamastur13@gmail.com>
2025-02-15 12:33:53 +0530

    New Practice set  added

---
+++ CodeWithHarry/05_Practice_set/All_Questions.js
@@ -0,0 +1,48 @@
+// Question 1 //
+// Create an array of numbers and take inout from the user to add  numbers to this array. //
+
+// let arr = [10,20,30,40];
+// let a = prompt("Enter your number");
+// a = Number.parseInt(a);
+// arr.push(a);
+// console.log(arr);
+
+// Question 2 //
+// Keep adding numbers to the array in until 0 is added to array //
+
+// let arr = [10, 20, 30, 40];
+// let a;
+// do {
+//     a = prompt("Enter your number");
+//     a = Number.parseInt(a);
+//     arr.push(a);
+//     console.log(arr);
+// } while (a != 0);
+
+// Question 3 //
+//filter for numbers divisible by 10 from a given array //
+
+// let arr = [1, 2, 30, 4, 5, 6, 7, 80, 90, 1];
+// let n = arr.filter((x)=>{
+//     return x % 10 == 0;
+// });
+// console.log(n); 
+
+//Question 4 //
+// create an array of square of given numbers //
+
+
+// let arr = [1, 2, 30, 4, 5, 6, 7, 80, 90, 1];
+//  let n = arr.map((x)=>{
+//      return x * x;
+//  });
+//  console.log(n); 
+
+//Question 5 //
+// use reduse to calculate factorial of a given number from an array of first n natural numbers //
+
+// let arr = [1,2,3,4,5];
+// let n = arr.reduce((x,y)=>{
+//     return x * y;
+// });
+// console.log(n);
