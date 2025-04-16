4fbe43278dcfc5931697964d4927e09962fb7741
Mastur <bikanerwalamastur13@gmail.com>
2025-04-16 14:00:45 +0530

    add some css

--- Red&white/Bill_project/script.js
+++ Red&white/Bill_project/script.js
@@ -1,17 +1,23 @@
 function calculate() {
     let amount = parseInt(document.getElementById('amount-input').value);
 
-    // console.log(`Amount : ${amount}`);
+
 
     let discount = parseInt(document.getElementById('discount-input').value);
-
+    let answerElement = document.getElementById('answer');
     let total = 0;
 
+    answerElement.classList.remove('show');
+
     if (isNaN(amount) || isNaN(discount) || amount < 0 || discount < 0) {
-        document.getElementById('answer').innerHTML = `Invalid data!`;
+        answerElement.innerHTML = `Invalid data!`;
     } else {
         total = amount - (amount * discount / 100);
-
-        document.getElementById('answer').innerHTML = `Total Bill : ${total}`;
+        answerElement.innerHTML = `Total Bill: ${total}`;
     }
+
+    void answerElement.offsetWidth;
+
+    // Add the show class to trigger the animation
+    answerElement.classList.add('show');
 }  
