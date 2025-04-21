5bc762df20f2a1142e3c97b42adb0e6b60a56704
Mastur <bikanerwalamastur13@gmail.com>
2025-04-21 11:02:27 +0530

    nothing added

--- s.js
+++ s.js
@@ -1,13 +1,59 @@
-let arr = [4,5,6,3,2,1];
+// let arr = [4,5,6,3,2,1];
 
-function isSorted(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[i - 1]) {
-      return false;
+// function isSorted(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] < arr[i - 1]) {
+//       return false;
+//     }
+//     return true;
+//   }
+// }
+
+// console.log(isSorted(arr));
+
+
+// function moveZeroes(arr) {
+//   let nonZeroIndex = 0;
+
+//   // Step 1: Move non-zero elements to the front
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== 0) {
+//       arr[nonZeroIndex] = arr[i];
+//       nonZeroIndex++;
+//     }
+//   }
+
+//   // Step 2: Fill remaining positions with 0
+//   for (let i = nonZeroIndex; i < arr.length; i++) {
+//     arr[i] = 0;
+//   }
+
+//   return arr;
+// }
+
+// // Example
+// console.log(moveZeroes([0, 1, 0, 3, 12])); // Output: [1, 3, 12, 0, 0]
+
+let arr = [0,1,21.0,46,35]
+
+function moveZeros(arr){
+  let nonZeroIndex = 0;
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] !== 0){
+        arr[nonZeroIndex] = arr[i];
+        nonZeroIndex++;
     }
-    return true;
-  }
+
 }
 
-console.log(isSorted(arr));
+for(let i = nonZeroIndex; i<arr.length ; i++ ){
+    arr[i] = 0;
 
+}
+
+return arr;
+
+}
+
+console.log(moveZeros(arr));
